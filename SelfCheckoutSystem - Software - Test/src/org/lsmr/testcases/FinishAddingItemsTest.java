72716f5b5d81fb5d132f42df7a3ce355431764d2
shreyap5 <60752941+shreyap5@users.noreply.github.com>
2021-03-31 19:50:56 -0600

    Update FinishAddingItemsTest.java

--- SelfCheckoutSystem - Software - Test/src/org/lsmr/testcases/FinishAddingItemsTest.java
+++ SelfCheckoutSystem - Software - Test/src/org/lsmr/testcases/FinishAddingItemsTest.java
@@ -125,23 +125,5 @@ public class FinishAddingItemsTest {
 		
     }
 	
-	// test to check if correct weight has been weighted if more item is added
-    @Test
-    public final void TestCorrectTotalWeightMoreItems() {
-        useCase = new FinishAddingItems();
-
-        ItemPrice1 = useCase.doneAddingItems();
-        ItemPrice2 = useCase.doneAddingItems();
-        ItemPrice3 = useCase.doneAddingItems();
-        ItemPrice4 = useCase.doneAddingItems();
-
-        ItemPrice5 = ItemPrice1.add(ItemPrice2);
-        ItemPrice5 = ItemPrice5.add(ItemPrice3);
-        ItemPrice5 = ItemPrice5.add(ItemPrice4);
-        useCase.updateCartTotal(ItemPrice5);
-
-        assertEquals(useCase.getCartTotal(), ItemPrice5);
-
-    }
 	
 }
